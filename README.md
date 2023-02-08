c6b0f584b0c1abe24d2b368315caa55699ec12b1
Andrea Sandoval <asandovala@fen.uchile.cl>
2023-02-08 04:35:31 -0300

    Entregable clase 32 en render

---
+++ README.md
@@ -0,0 +1,5 @@
+Dependencias del proyecto:
+
+npm install compression connect-mongo dotenv ejs express express-session knex log4js minimist mysql nodemon normalizr passport passport-facebook passport-local socket.io @faker-js/faker
+
+npm install autocanon -D
---
+++ src/api/productos.js
@@ -0,0 +1,11 @@
+import config from '../config.js'
+
+import ContenedorArchivo from '../contenedores/ContenedorArchivo.js'
+// import ContenedorMongoDb from '../contenedores/ContenedorMongoDb.js'
+
+const productosApi = new ContenedorArchivo(`${config.fileSystem.path}/productos.json`)
+// const productosApi2 = new ContenedorMongoDb('productos', {
+//     productos: { type: [], required: true }
+// })
+
+export default productosApi
